0101c864159e3c7fd444360013b4cdc78dffb9ca
SYAM T S <141516071+Syam543@users.noreply.github.com>
2023-11-08 18:23:09 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+
+
+
+document.querySelector(".this").style.color = "purple"
+document.querySelector(".this").style.background = "green"
+
+
+const tvShowsLink = document.getElementById("features");
+
+tvShowsLink.addEventListener("click", function() {
+  window.location.href = "TvShows.html";
+});
+
+
